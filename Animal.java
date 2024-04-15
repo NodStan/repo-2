004ad29bfe7ba3220588fb6b73979ee9281fb445
Stanley <stanleyobitunwase@gmail.com>
2024-04-15 13:15:17 +0100

    repo2

---
+++ Animal.java
@@ -0,0 +1,13 @@
+package repo2;
+
+public class Animal{
+
+    public void eat(){
+        System.out.println("sad asf");
+    }
+
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.eat();
+    }
+}
